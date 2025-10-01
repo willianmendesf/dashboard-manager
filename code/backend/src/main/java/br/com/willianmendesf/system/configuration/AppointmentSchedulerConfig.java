6c6a8851e617ef1ae46008ca941b507101d73eac
willianmendesf <robertinho1190@gmail.com>
2025-10-01 20:00:03 -0300

    update cron scheduler to run at 12 times

--- code/backend/src/main/java/br/com/willianmendesf/system/configuration/AppointmentSchedulerConfig.java
+++ code/backend/src/main/java/br/com/willianmendesf/system/configuration/AppointmentSchedulerConfig.java
@@ -2,46 +2,33 @@ package br.com.willianmendesf.system.configuration;
 
 import br.com.willianmendesf.system.service.AppointmentSchedulerService;
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
 @Configuration
 @EnableScheduling
-@Slf4j
+@AllArgsConstructor
 public class AppointmentSchedulerConfig {
     private final AppointmentSchedulerService schedulerService;
 
-    @Autowired
-    public AppointmentSchedulerConfig(AppointmentSchedulerService schedulerService) {
-        this.schedulerService = schedulerService;
-    }
-
-    /**
-     * Carrega os agendamentos para o cache uma vez por dia (à meia-noite)
-     */
-    @Scheduled(cron = "*/10 * * * * ?")
-    public void dailyAppointmentLoading() {
-        log.info("Iniciando carregamento diário de agendamentos");
+    @Scheduled(cron = "${scheduler.cron.scheduleTimeLoad}")
+    public void scheduleAppointmentLoad() {
+        log.info("Loading appointment scheduled");
         schedulerService.loadAppointmentsToCache();
     }
 
-    /**
-     * Verifica agendamentos a cada segundo
-     */
     @Scheduled(fixedRate = 1000)
     public void checkScheduledAppointments() {
         schedulerService.checkAndExecuteScheduledAppointments();
     }
 
-    /**
-     * Carrega os agendamentos na inicialização da aplicação
-     */
     @PostConstruct
     public void init() {
-        log.info("Inicializando o sistema de agendamento");
+        log.info("Starting appointment scheduled");
         schedulerService.loadAppointmentsToCache();
     }
 }
