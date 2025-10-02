506b32cc7b73823d72e235afeeaace53a8f95df0
willianmendesf <robertinho1190@gmail.com>
2025-10-02 18:03:41 -0300

    added docker backend api

--- code/frontend/src/environments/environment.ts
+++ code/frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/api/v1/'
-  // apiUrl: 'http://localhost:8080/api/v1/'
+  //apiUrl: 'http://localhost/api/v1/'
+  apiUrl: 'http://backend-app:8080/api/v1/'
 };
